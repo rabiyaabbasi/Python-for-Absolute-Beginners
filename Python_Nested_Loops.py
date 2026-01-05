66ca77ff8adab08efcc932bc4d94492d90cbaf7e
Rabiya Abbasi <54917838+rabiyaabbasi@users.noreply.github.com>
2026-01-05 13:15:54 -0800

    Python_Practice_Problems
    Nested For Loops, Modules and Imports, User Input and Functions

---
+++ Python_Nested_Loops.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3]
+
+letters = ["a", "b", "c"]
+
+for number in numbers: 
+    number
+    for letter in letters:
+        number
+        letter
+        print(f"{number}{letter}", end= " ")
+    print()
---
+++ calculator.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    c = a+b
+    return(c)
+
+def subtract(a, b):
+    d = a-b
+    return(d)
+
+if __name__ == "__main__":
+    x = add(4,3)
+    print(x)
